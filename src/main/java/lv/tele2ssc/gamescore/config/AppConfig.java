2accc4f28158dccb410b462f481c28a4d1e507d4
vitasava <vitasava@T2010176.corp.tele2.com>
2017-11-02 22:43:51 +0200

    Initial template setup. LayoutDialect.

---
+++ src/main/java/lv/tele2ssc/gamescore/config/AppConfig.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lv.tele2ssc.gamescore.config;
+
+import nz.net.ultraq.thymeleaf.LayoutDialect;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+
+/**
+ * Web application configuration. We need this to add interceptor which will
+ * select current logged-in user and put it to the model of every page.
+ */
+@Configuration
+public class AppConfig extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        // Registering interceptor
+        //  registry.addInterceptor(currentUserInterceptor);
+    }
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+       registry.addViewController("/").setViewName("index");
+       registry.addViewController("/ping-pong").setViewName("ping-pong");
+       registry.addViewController("/novus").setViewName("novus");
+       registry.addViewController("/football").setViewName("football");
+       registry.addViewController("/pull-bar").setViewName("pull-bar");
+        
+    }
+
+    @Bean
+    public LayoutDialect layoutDialect() {
+        return new LayoutDialect();
+    }
+
+}
---
+++ src/main/java/lv/tele2ssc/gamescore/config/SecurityConfiguration.java
@@ -0,0 +1,66 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lv.tele2ssc.gamescore.config;
+
+import javax.sql.DataSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+/**
+ *
+ * @author vitasava
+ */
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private DataSource dataSource;
+    @Autowired()
+    BCryptPasswordEncoder passwordEncoder; 
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        /*
+        auth.jdbcAuthentication()
+                .usersByUsernameQuery("select email, password, 1 from user where email=?")
+                .authoritiesByUsernameQuery("select u.email,r.name from user u, user_role r,user_role_permission urp "
+                        + "where u.id=urp.user_id and r.id=urp.user_role_id and u.email=?")
+                .dataSource(dataSource)
+                .passwordEncoder(NoOpPasswordEncoder.getInstance());
+        //.passwordEncoder(passwordEncoder);
+*/
+
+//To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/*").permitAll()
+                .antMatchers("/assets/**").permitAll()                
+                .antMatchers("/login/**").permitAll()
+                .antMatchers("/user/**").authenticated()
+                .antMatchers("/admin/**").hasAnyAuthority("admin")
+                .antMatchers("/h2-console/**").permitAll()
+                .anyRequest().authenticated()
+                .and().cors()
+                .and().csrf().disable().formLogin().loginPage("/login").usernameParameter("username").passwordParameter("password")
+                .and().headers().frameOptions().disable();
+
+    }
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder(){        
+    	return new BCryptPasswordEncoder();
+    }
+}
