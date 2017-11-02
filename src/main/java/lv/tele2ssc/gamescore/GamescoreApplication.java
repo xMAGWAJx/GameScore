abd8297bb1dd9b429286a063e1ccd67c4d8746f0
zantok <zantok1991@gmail.com>
2017-11-02 14:45:51 +0200

    first version

---
+++ src/main/java/lv/tele2ssc/gamescore/GamescoreApplication.java
@@ -0,0 +1,12 @@
+package lv.tele2ssc.gamescore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamescoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamescoreApplication.class, args);
+	}
+}
